ab8c2442b9ebb19f5fb4aa71d3088a59a2224373
Moritz Hofer <moritz.hofer@xitaso.com>
2026-06-09 14:39:33 +0200

    Replace Microsoft.AspNetCore.WebUtilities with BCL Base64Url
    Use System.Buffers.Text.Base64Url on net9/net10 with a manual polyfill
    for net8.0 to remove the ASP.NET Core hosting dependency from the core
    library.

--- MnestixCore/Shared/Base64StringDeAndEncoder.cs
+++ MnestixCore/Shared/Base64StringDeAndEncoder.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+#if NET8_0
+using System.Buffers;
+#else
+using System.Buffers.Text;
+#endif
 
 namespace MnestixCore.Shared;
 
@@ -6,15 +11,45 @@ public static class Base64StringDeAndEncoder
 {
     public static string DecodeFrom64(string encodedData)
     {
-        var encodedDataAsBytes = WebEncoders.Base64UrlDecode(encodedData);
-        var returnValue = System.Text.Encoding.ASCII.GetString(encodedDataAsBytes);
-        return returnValue;
+        var encodedDataAsBytes = Base64UrlDecode(encodedData);
+        return Encoding.ASCII.GetString(encodedDataAsBytes);
     }
 
     public static string EncodeTo64(string toEncode)
     {
-        var toEncodeAsBytes = System.Text.Encoding.ASCII.GetBytes(toEncode);
-        var returnValue = WebEncoders.Base64UrlEncode(toEncodeAsBytes);
-        return returnValue;
+        var toEncodeAsBytes = Encoding.ASCII.GetBytes(toEncode);
+        return Base64UrlEncode(toEncodeAsBytes);
     }
+
+#if NET8_0
+    private static byte[] Base64UrlDecode(string input)
+    {
+        var output = input.Replace('-', '+').Replace('_', '/');
+        switch (output.Length % 4)
+        {
+            case 2: output += "=="; break;
+            case 3: output += "="; break;
+        }
+
+        return Convert.FromBase64String(output);
+    }
+
+    private static string Base64UrlEncode(byte[] input)
+    {
+        return Convert.ToBase64String(input)
+            .TrimEnd('=')
+            .Replace('+', '-')
+            .Replace('/', '_');
+    }
+#else
+    private static byte[] Base64UrlDecode(string input)
+    {
+        return Base64Url.DecodeFromChars(input);
+    }
+
+    private static string Base64UrlEncode(byte[] input)
+    {
+        return Base64Url.EncodeToString(input);
+    }
+#endif
 }
