9a8ff51ebad1eb682aba0bf7a80bd7e841d222f1
artacho-xitaso <jose.artacho@xitaso.com>
2026-06-18 15:06:18 +0200

    fix: show enum values for asset kind in swagger

--- MnestixCore/Dtos/AssetKind.cs
+++ MnestixCore/Dtos/AssetKind.cs
@@ -1,9 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace MnestixCore.Dtos;
 
 /// <summary>
 /// AssetKind enum as defined in AAS specification.
 /// Describes whether an AAS represents a Type (template), Instance (concrete asset), or is NotApplicable.
 /// </summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum AssetKind
 {
     Instance,
